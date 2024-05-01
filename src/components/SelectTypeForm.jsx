08e9f903834d4aa0968490f13612dc506ff99087
homonoviscoding <mama.leye@gmail.com>
2024-05-01 11:42:18 +0100

    Trying but cannot figure out something regarding the data type

--- src/components/SelectTypeForm.jsx
+++ src/components/SelectTypeForm.jsx
@@ -1,10 +1,16 @@
-function SelectTypeForm(props) {
+function SelectTypeForm({setDataType}) {
   // Write code here...
+
+  const handleChange = (e) =>{
+    e.target.value
+
+    setDataType
+  }
 
   return (
     <form className="three-column-grid__expand-two gap-md">
       <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
+      <select onChange={handleChange} id="type" name="type">
         <option value="">Please select...</option>
         <option value="people">People</option>
         <option value="planets">Planets</option>
